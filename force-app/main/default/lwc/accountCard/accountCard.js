8c8d761afd0904707ff8d20852295c17b5096a79
Justin Lyon <jml6487@gmail.com>
2019-05-09 08:28:17 -0500

    add account grid and card

---
+++ force-app/main/default/lwc/accountCard/accountCard.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class AccountCard extends LightningElement {
+  @api account
+}
--- force-app/main/default/lwc/homeAccountApp/homeAccountApp.js
+++ force-app/main/default/lwc/homeAccountApp/homeAccountApp.js
@@ -1,5 +1,14 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, track, wire } from 'lwc'
+import getLimittedAccounts from '@salesforce/apex/HomeAccountAuraService.getLimittedAccounts'
 
 export default class HomeAccountApp extends LightningElement {
   @api limitter
+  @api accounts
+  @track error
+
+  @wire(getLimittedAccounts, { limitter: '$limitter' })
+  handleWiredAccounts({ error, data }) {
+    this.accounts = data
+    this.error = error
+  }
 }
