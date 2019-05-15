58a38c1b6027015847b7fd23daa071855ca51baa
Justin Lyon <jml6487@gmail.com>
2019-05-15 13:34:02 -0500

    add clickable account cards

--- force-app/main/default/lwc/accountCard/accountCard.js
+++ force-app/main/default/lwc/accountCard/accountCard.js
@@ -2,4 +2,12 @@ import { LightningElement, api } from 'lwc';
 
 export default class AccountCard extends LightningElement {
   @api account
+
+  clickAccount() {
+    const selectEvent = new CustomEvent('accountclick', {
+      bubbles: true,
+      detail: this.account.Id
+    })
+    this.dispatchEvent(selectEvent)
+  }
 }
--- force-app/main/default/lwc/homeAccountApp/homeAccountApp.js
+++ force-app/main/default/lwc/homeAccountApp/homeAccountApp.js
@@ -1,7 +1,8 @@
+import { NavigationMixin } from 'lightning/navigation'
 import { LightningElement, api, track, wire } from 'lwc'
 import getLimittedAccounts from '@salesforce/apex/HomeAccountAuraService.getLimittedAccounts'
 
-export default class HomeAccountApp extends LightningElement {
+export default class HomeAccountApp extends NavigationMixin(LightningElement) {
   @api limitter
   @api accounts
   @track error
@@ -10,5 +11,18 @@ export default class HomeAccountApp extends LightningElement {
   handleWiredAccounts({ error, data }) {
     this.accounts = data
     this.error = error
+  }
+
+  gotoAccount(event) {
+    event.stopPropagation()
+
+    this[NavigationMixin.Navigate]({
+      type: 'standard__recordPage',
+      attributes: {
+        recordId: event.detail,
+        objectApiName: 'Account',
+        actionName: 'view'
+      }
+    })
   }
 }
