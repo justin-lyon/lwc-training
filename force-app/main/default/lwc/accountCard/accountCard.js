90256f2d3435bc37a49ad4641da55235bab570a5
Justin Lyon <jml6487@gmail.com>
2019-05-20 14:44:30 -0500

    clean up account card

--- force-app/main/default/lwc/accountCard/accountCard.js
+++ force-app/main/default/lwc/accountCard/accountCard.js
@@ -2,16 +2,4 @@ import { LightningElement, api } from 'lwc';
 
 export default class AccountCard extends LightningElement {
   @api account
-
-  gotoAccount() {
-    this.dispatchClick('navaccount', this.account.Id)
-  }
-
-  dispatchClick(name, id) {
-    const event = new CustomEvent(name.toLowerCase(), {
-      bubbles: true,
-      detail: id
-    })
-    this.dispatchEvent(event)
-  }
 }
