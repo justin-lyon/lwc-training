724fbc4aef4b9ba88cfcff7ebef2705f291bef40
Justin Lyon <jml6487@gmail.com>
2019-05-23 09:58:07 -0500

    refactor related contacts to list

--- force-app/main/default/lwc/contactItem/contactItem.js
+++ force-app/main/default/lwc/contactItem/contactItem.js
@@ -1,3 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
-export default class ContactItem extends LightningElement {}
+const stockPhoto = 'https://www.lightningdesignsystem.com/assets/images/avatar2.jpg'
+
+export default class ContactItem extends LightningElement {
+  @api contact
+
+  getAvatar() {
+    if (this.contact.PhotoUrl) return this.contact.PhotoUrl
+
+    return stockPhoto
+  }
+
+  clickContact() {
+    this.dispatchClick('gotoContact', this.contact.Id)
+  }
+
+  dispatchClick(name, id) {
+    const event = new CustomEvent(name.toLowerCase(), {
+      bubbles: true,
+      detail: id
+    })
+    this.dispatchEvent(event)
+  }
+}
