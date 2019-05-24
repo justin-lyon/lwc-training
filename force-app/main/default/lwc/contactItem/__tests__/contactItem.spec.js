1141d97ff7c613f65b427d92ffd4d21775c63d8d
Justin Lyon <jml6487@gmail.com>
2019-05-24 14:35:22 -0500

    add contactItem test

---
+++ force-app/main/default/lwc/contactItem/__tests__/contactItem.spec.js
@@ -0,0 +1,31 @@
+import { createElement } from 'lwc'
+import ContactItem from 'c/contactItem'
+
+const NAME = 'Rick Sanchez'
+const EMAIL = 'rick.sanchez@thecouncil.net'
+const CONTACT = {
+  Name: NAME,
+  Email: EMAIL
+}
+
+describe('c-contact-item', () => {
+  afterEach(() => {
+    // The jsdom instance is shared across test cases in a single file so reset the DOM.
+    while (document.body.firstChild) {
+      document.body.removeChild(document.body.firstChild)
+    }
+  })
+
+  it('has a contact', () => {
+    // create element
+    const element = createElement('c-contact-item', {
+      is: ContactItem
+    })
+    element.contact = CONTACT
+    document.body.appendChild(element)
+
+    // verify contact record
+    const span = element.shadowRoot.querySelector('span.clickable')
+    expect(span.textContent).toBe(NAME)
+  })
+})
