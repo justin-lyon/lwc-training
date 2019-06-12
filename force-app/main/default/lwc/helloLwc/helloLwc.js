9b12e1776bef357bf22c512a291be24d0c2a664f
Justin Lyon <jml6487@gmail.com>
2019-06-12 12:35:42 -0500

    add Greeting to Hello Page

--- force-app/main/default/lwc/helloLwc/helloLwc.js
+++ force-app/main/default/lwc/helloLwc/helloLwc.js
@@ -1,3 +1,5 @@
-import { LightningElement } from 'lwc'
+import { LightningElement, api } from 'lwc'
 
-export default class HelloLwc extends LightningElement {}
+export default class HelloLwc extends LightningElement {
+  @api greeting
+}
